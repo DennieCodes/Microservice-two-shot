d7bb3c58e3f84a423f104ba91d46397083521e39
wlongmire <warrenlongmire@gmail.com>
2023-01-20 17:30:33 -0500

    changes location to bin.

--- wardrobe/api/wardrobe_api/models.py
+++ wardrobe/api/wardrobe_api/models.py
@@ -23,7 +23,7 @@ class Bin(models.Model):
     bin_size = models.PositiveSmallIntegerField()
 
     def get_api_url(self):
-        return reverse("api_location", kwargs={"pk": self.pk})
+        return reverse("api_bin", kwargs={"pk": self.pk})
 
     def __str__(self):
         return f"{self.closet_name} - {self.bin_number}/{self.bin_size}"
