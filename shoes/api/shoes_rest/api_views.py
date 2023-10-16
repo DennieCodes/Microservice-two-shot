77fcc8a60c3502f677e27127c4755533612ad2ce
Dennie <oculareo@gmail.com>
2023-10-16 16:02:44 -0400

    Added encoder and restored functionality to get, post requests

--- shoes/api/shoes_rest/api_views.py
+++ shoes/api/shoes_rest/api_views.py
@@ -1,15 +1,31 @@
+import json
 from .models import Shoe
 from django.http import JsonResponse
+from common.json import ModelEncoder
 from django.views.decorators.http import require_http_methods
 # from django.shortcuts import render
 
+class ShoeListEncoder(ModelEncoder):
+  model = Shoe
+  properties = ["name"]
+
 # API_LIST_SHOES
-@require_http_methods(["GET"])
+@require_http_methods(["GET", "POST"])
 def api_list_shoes(request):
-  # shoes = Shoe.objects.all()
+  if request.method == "GET":
+    shoes = Shoe.objects.all()
 
-  shoes = { "name": "Nike"}
+    return JsonResponse(
+      { "shoes": shoes },
+      encoder=ShoeListEncoder,
+      safe=False
+    )
+  else:
+    content = json.loads(request.body)
 
-  return JsonResponse(
-    { "shoes": shoes },
-  )
+    shoe = Shoe.objects.create(**content)
+    return JsonResponse(
+      shoe,
+      encoder=ShoeListEncoder,
+      safe=False,
+    )
